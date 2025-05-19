7b85ed7e4dfcc2aa3245734f47a129829d5b5af6
Abhinav Nair <abhinav.nair@vayana.com>
2025-05-19 12:11:20 +0530

    added photoservice

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { PhotoService } from '../services/photo.service';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -7,7 +7,8 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab2Page {
-
-  constructor() {}
-
+  constructor(public photoService: PhotoService) {}
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 }
