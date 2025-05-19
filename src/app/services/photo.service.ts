019413c4bddfe301a9da007b8b2f255c891c5155
Abhinav Nair <abhinav.nair@vayana.com>
2025-05-19 12:34:10 +0530

    added photo service

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -5,6 +5,7 @@ import {
   CameraSource,
   Photo,
 } from '@capacitor/camera';
+import { WebView } from '@capacitor/core';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
 @Injectable({
@@ -13,6 +14,34 @@ import { Preferences } from '@capacitor/preferences';
 export class PhotoService {
   public photos: UserPhoto[] = [];
   constructor() {}
+  private convertBlobToBase64 = (blob: Blob) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    });
+  private async readAsBase64(photo: Photo) {
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+    return (await this.convertBlobToBase64(blob)) as string;
+  }
+  private async savePicture(photo: Photo) {
+    const base64Data = await this.readAsBase64(photo);
+
+    const fileName = Date.now() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+    return {
+      filePath: fileName,
+      webViewPath: photo.webPath!,
+    };
+  }
   public async addNewToGallery() {
     //Take a photo
     const capturedPhoto = await Camera.getPhoto({
@@ -20,10 +49,8 @@ export class PhotoService {
       source: CameraSource.Camera,
       quality: 100,
     });
-    this.photos.unshift({
-      filePath: 'soon',
-      webViewPath: capturedPhoto.webPath!,
-    });
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    this.photos.unshift(savedImageFile);
   }
 }
 export interface UserPhoto {
